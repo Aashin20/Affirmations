cfcce16beefcf6ab8d4d105e6eeea4956b88531e
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-11-05 08:37:14 +0530

    Minor fix

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ app = FastAPI()
 
 @app.get("/affimations/{mood}")
 async def fetch_affirmation(mood: str):
+
     return get_affirmations(mood)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
